59e5d5c80f42ddc1591d9d1a83e2dae7c9b19cda
rahulaitian <93325608+rahulaitian@users.noreply.github.com>
2025-09-27 15:10:06 +0530

    Extend calculator with history log for operations.

--- src/main.py
+++ src/main.py
@@ -1,19 +1,42 @@
-def basic_calculator(a, b, operation):
-    try:
-        if operation == "addition":
-            return a + b
-        elif operation == "subtraction":
-            return a - b
-        elif operation == "multiplication":
-            return a * b
-        elif operation == "division":
-            return a / b
-        else:
-            return "Invalid operation"
-    except ZeroDivisionError:
-        return "Cannot divide by zero"
+class Calculator:
+    def __init__(self):
+        self.history = []
 
-print(basic_calculator(10, 2, "addition"))
-print(basic_calculator(10, 2, "subtraction"))
-print(basic_calculator(10, 2, "multiplication"))
-print(basic_calculator(10, 0, "division"))
+    def perform_operation(self, a, b, operation):
+        try:
+            if operation == "addition":
+                result = a + b
+                self.history.append(f'{a} + {b} = {result}')
+                return result
+
+            elif operation == "subtraction":
+                result = a - b
+                self.history.append(f'{a} - {b} = {result}')
+                return result
+
+            elif operation == "multiplication":
+                result = a * b
+                self.history.append(f'{a} * {b} = {result}')
+                return result
+
+            elif operation == "division":
+                result = a / b
+                self.history.append(f'{a} / {b} = {result}')
+                return result
+
+            else:
+                return "Invalid operation"
+
+        except ZeroDivisionError:
+            return "Cannot divide by zero"
+
+    def print_history(self):
+        for operation in self.history:
+            print(operation)
+
+cal = Calculator()
+print(cal.perform_operation(10, 2, "addition"))
+print(cal.perform_operation(10, 2, "subtraction"))
+print(cal.perform_operation(10, 2, "multiplication"))
+print(cal.perform_operation(10, 0, "division"))
+cal.print_history()
