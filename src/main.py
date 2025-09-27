b0dca3fee496f28f958ee89ed92732cd204ba953
rahulaitian <93325608+rahulaitian@users.noreply.github.com>
2025-09-27 11:43:37 +0530

    Remove add and subtract methods from calculator, add cube method

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
+class Calculator:
+    def multiply(self, a, b):
+        return a * b
 
+    def divide(self, a, b):
+        if b == 0:
+            raise ValueError("Can't divide by zero!")
+        return a / b
+
+    def cube(self, a):
+        return a**3
